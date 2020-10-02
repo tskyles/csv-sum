cb3bdc4098d637066f557643b87516091c92104d
travis skyles <travis.skyles@gmail.com>
2020-10-02 14:54:33 -0700

    Created FileSelectToString

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import { FileSelector } from './components/FileSelector/FileSelector';
 
 function App() {
+
+  
   return (
     <div className='app-container'>
-      {/* <FileSelector/> */}
+      <FileSelector 
+        fileType='text/csv' 
+        onLoad={(string) => console.log(string)} 
+        onError={(e) => console.error(e)}
+      />
       <button>Sum Values</button>
       {/* <SumDisplay/> */}
     </div>
--- src/components/FileSelector/FileSelector.tsx
+++ src/components/FileSelector/FileSelector.tsx
@@ -1,3 +1,39 @@
+import React, { FC } from 'react';
 
+type FileSelectToStringProps = {
+  fileType: string;
+  onLoad: (csvString: string | ArrayBuffer) => void;
+  onError: (error: Event) => void;
+};
 
-export { };
+export const FileSelectToString: FC<FileSelectToStringProps> = ({ fileType, onLoad, onError}) => {
+
+  // get csv file and read it
+  function selectFile(fileList: FileList | null) {
+    if(!fileList) return;
+    const file = fileList[0]
+    // get the file info
+
+    if (file.type !== fileType) {
+      console.error('Wrong File Type');
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if(!reader.result) return;
+      onLoad(reader.result);
+    };
+    reader.onerror = (e) => onError(e);
+
+    reader.readAsText(file);
+  }
+
+  return (
+    <div className="file-select-container">
+      <label>Upload .csv file:</label>
+      <input type='file' onChange={e => selectFile(e.target.files)}></input>
+    </div>
+  )
+}
